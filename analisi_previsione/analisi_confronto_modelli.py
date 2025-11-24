a6069c3853ff0caf39968d6f0266e5902de30811
maselli-ai <maselli.s@outlook.it>
2025-11-24 15:56:49 +0100

    Update analisi_confronto_modelli.py

--- analisi_previsione/analisi_confronto_modelli.py
+++ analisi_previsione/analisi_confronto_modelli.py
@@ -3,16 +3,17 @@ from pathlib import Path
 
 OUT_DIR = Path("../output_311")
 
-# Importa le performance dei vari modelli
+# Importo le performance dei vari modelli
 sma_wma = pd.read_excel(OUT_DIR / "SMA_WMA_performance.xlsx")
 arima_prophet = pd.read_excel(OUT_DIR / "ARIMA_Prophet_performance.xlsx")
 
-# Unisci in un’unica tabella
+# Unisco in un’unica tabella
 all_perf = pd.concat([sma_wma, arima_prophet], ignore_index=True)
 
-# Calcola la media per modello
+# Calcolo la media per modello
 summary = all_perf.groupby("modello")[["MAE", "RMSE", "MAPE(%)"]].mean().round(2)
 summary = summary.sort_values("MAPE(%)")
 
 print(summary)
 summary.to_excel(OUT_DIR / "Confronto_Modelli.xlsx")
+
